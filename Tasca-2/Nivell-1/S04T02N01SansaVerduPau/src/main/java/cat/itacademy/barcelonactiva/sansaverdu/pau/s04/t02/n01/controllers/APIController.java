35b9d1dc060a855c823527787315f5a2888809b4
Sansa <pausansa59@gmail.com>
2023-03-27 12:23:24 +0200

    Add nivell 2

---
+++ Tasca-2/Nivell-1/S04T02N01SansaVerduPau/src/main/java/cat/itacademy/barcelonactiva/sansaverdu/pau/s04/t02/n01/controllers/APIController.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.sansaverdu.pau.s04.t02.n01.controllers;public class APIRepo {
+}
---
+++ Tasca-2/Nivell-1/S04T02N01SansaVerduPau/src/main/java/cat/itacademy/barcelonactiva/sansaverdu/pau/s04/t02/n01/model/services/FruitaService.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.sansaverdu.pau.s04.t02.n01.model.services;public class FruitaService {
+}
